2a9b4d96aad69d34ca216c79ad13ca0d52131315
tutinoco <m@tutino.co>
2023-01-01 16:38:22 +0900

    ミニスクリーンが配置されている箇所に説明書きを追加
    ミニスクリーン説明書きの追加により新しいテクスチャとマテリアルを作成しました。
    座布団の高さを変更した際に他のユーザからはY軸以外も移動して見える不具合を修正しました。

--- Assets/VRGO/Scripts/Zabuton.cs
+++ Assets/VRGO/Scripts/Zabuton.cs
@@ -16,10 +16,11 @@ public class Zabuton : UdonSharpBehaviour
     void Update()
     {
         Vector3 p = gameObject.transform.localPosition;
+        p = new Vector3(0,Mathf.Max(Mathf.Min(p.y,0.35f),0.0f),0);
+        model.gameObject.transform.localPosition = p;
         if ( Networking.IsOwner(gameObject) ) {
-            gameObject.transform.localPosition = new Vector3(0,Mathf.Max(Mathf.Min(p.y,0.35f),0.0f),0);
+            gameObject.transform.localPosition = p;
             gameObject.transform.localRotation = Quaternion.Euler(0,0,0);
         }
-        model.gameObject.transform.localPosition = gameObject.transform.localPosition;
     }
 }
